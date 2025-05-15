8b94b641925d9b3bb3fd703e6b081f08be742445
FrancescoTonelli <francesco.tonelli9@studio.unibo.it>
2025-05-15 11:17:23 +0200

    MainActivity debug removed

--- Hitwaves/app/src/main/java/com/hitwaves/MainActivity.kt
+++ Hitwaves/app/src/main/java/com/hitwaves/MainActivity.kt
@@ -17,40 +17,31 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
-        val appDebug = true
-
         setContent {
             HitwavesTheme {
                 SplashScreen()
             }
         }
 
-        if (appDebug) {
-            val intent = Intent(this@MainActivity, AppActivity::class.java)
+        val token = TokenManager.getToken()
+
+        if (token != null) {
+            lifecycleScope.launch {
+                val (success, response) = apiAutomaticLogin()
+                if (success) {
+                    val intent = Intent(this@MainActivity, AppActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                } else {
+                    val intent = Intent(this@MainActivity, LoginActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+            }
+        } else {
+            val intent = Intent(this@MainActivity, LoginActivity::class.java)
             startActivity(intent)
             finish()
-        }
-        else {
-            val token = TokenManager.getToken()
-
-            if (token != null) {
-                lifecycleScope.launch {
-                    val (success, response) = apiAutomaticLogin()
-                    if (success) {
-                        val intent = Intent(this@MainActivity, AppActivity::class.java)
-                        startActivity(intent)
-                        finish()
-                    } else {
-                        val intent = Intent(this@MainActivity, LoginActivity::class.java)
-                        startActivity(intent)
-                        finish()
-                    }
-                }
-            } else {
-                val intent = Intent(this@MainActivity, LoginActivity::class.java)
-                startActivity(intent)
-                finish()
-            }
         }
 
 
