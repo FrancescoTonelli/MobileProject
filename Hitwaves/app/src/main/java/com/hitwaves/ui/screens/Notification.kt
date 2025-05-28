f7897e2e52952f13914387f0bab9bbfb10dfe4e4
FrancescoTonelli <francesco.tonelli9@studio.unibo.it>
2025-05-28 14:27:51 +0200

    Update Notification.kt

--- Hitwaves/app/src/main/java/com/hitwaves/ui/screens/Notification.kt
+++ Hitwaves/app/src/main/java/com/hitwaves/ui/screens/Notification.kt
@@ -1,14 +1,18 @@
 package com.hitwaves.ui.screens
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.net.Uri
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -17,8 +21,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat
 import androidx.navigation.NavHostController
 import com.hitwaves.ui.component.CustomSnackBar
 import com.hitwaves.ui.component.NotificationCard
@@ -39,6 +45,8 @@ fun Notification(navController: NavHostController) {
     val isLoading by notificationViewModel.isNotificationLoading
     val readState by notificationViewModel.readState
     val snackBarHostState = remember { SnackbarHostState() }
+
+    val context = LocalContext.current
 
     LaunchedEffect(Unit) {
         notificationViewModel.getNotifications()
@@ -94,11 +102,33 @@ fun Notification(navController: NavHostController) {
                 }
             }
 
+            if (
+                ContextCompat.checkSelfPermission(context, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED
+            ) {
+                item {
+                    Spacer(modifier = Modifier.padding(8.dp))
+                    HorizontalDivider(
+                        thickness = 1.dp,
+                        color = Secondary,
+                        modifier = Modifier.fillMaxWidth(0.9f)
+                    )
+                    Spacer(modifier = Modifier.padding(8.dp))
+                    Text(
+                        text = "Notifications are disabled. Enable them to stay updated.",
+                        style = Typography.bodyLarge.copy(color = Secondary),
+                        modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp)
+                    )
+                }
+            }
+
             item {
                 Spacer(modifier = Modifier.padding(16.dp))
             }
 
+
         }
+
+
     }
 
     CustomSnackBar(snackBarHostState)
